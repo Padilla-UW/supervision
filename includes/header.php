ff9779c5fdefb789eaa298bb409d2b2f1e47cabc
Padilla <hector.padilla@uwipes.com>
2022-10-18 11:55:57 -0500

    Se agrego el titulo

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Supervición</title>
     <link rel="icon" href="./assets/img/logoUwipes.png">
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
